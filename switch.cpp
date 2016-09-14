198cf0a27cb886c2a5eb15c137318a4bf5892949
lijinping <li.jinping@zte.com.cn>
2016-09-14 20:02:24 +0800

    Update switch.cpp

--- switch.cpp
+++ switch.cpp
@@ -16,7 +16,7 @@ int main()
   switch(isw)    {
                
     case one:                            //just lables
-         int a = 0;
+         int a = 0;  //a在这里定义，有效期到switch的}，如果直接走了case2，此变量是没有定义的 ，所以编译不过.
          break;                       //trans to goto statement
     case two:
          break;
